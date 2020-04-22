3200bcff0cad5b18572c3dc5c2bf3b21dd7135f5
Martin Štěpánek <mstepanek015@gmail.com>
2020-04-22 10:03:56 +0200

    feat(about-us): add about us page

--- BasicEshop/BasicEshop/Controllers/HomeController.cs
+++ BasicEshop/BasicEshop/Controllers/HomeController.cs
@@ -19,6 +19,11 @@ namespace BasicEshop.Controllers
             return View();
         }
 
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
