2636db18d112b14598cd20f125e062753876b65d
Martin Štěpánek <mstepanek015@gmail.com>
2020-04-25 21:35:05 +0200

    feat(homepage): add list of products to homepage

--- BasicEshop/BasicEshop/Controllers/HomeController.cs
+++ BasicEshop/BasicEshop/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Diagnostics;
+using System.Linq;
 using BasicEshop.Models;
 using BasicEshop.Models.Database;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BasicEshop.Controllers
 {
@@ -16,6 +18,12 @@ namespace BasicEshop.Controllers
 
         public IActionResult Index()
         {
+            ViewBag.Products = _context.Products
+                .Include(x => x.Images)
+                .ThenInclude(image => image.Image)
+                .Take(8)
+                .ToList();
+
             return View();
         }
 
